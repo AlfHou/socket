874cdb1b5803ee3e4cd7dbad81e896e9bea0ce9c
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-03-26 10:36:53 +0100

    Refactor server.c

--- server.c
+++ server.c
@@ -8,39 +8,34 @@
 #include <unistd.h>
 
 #define MYPORT "8777"
-
-int main(void) {
+int get_port(struct addrinfo** server_info) {
 	struct addrinfo hints;
-	struct addrinfo* server_info;
-	int status;
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	if ((status = getaddrinfo(NULL, MYPORT, &hints, &server_info)) != 0) {
+	int status;
+	if ((status = getaddrinfo(NULL, MYPORT, &hints, server_info)) != 0) {
 		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 		exit(EXIT_FAILURE);
 	}
 
-	int my_socket = socket(server_info -> ai_family, server_info -> ai_socktype,
-		server_info -> ai_protocol);
+	int my_socket = socket((*server_info) -> ai_family, (*server_info) -> ai_socktype,
+		(*server_info) -> ai_protocol);
 	if (my_socket == -1) {
 		perror("Socket");
 		exit(EXIT_FAILURE);
 	}
-	if (bind(my_socket, server_info -> ai_addr, server_info -> ai_addrlen) == -1) {
+	if (bind(my_socket, (*server_info) -> ai_addr, (*server_info) -> ai_addrlen) == -1) {
 		close(my_socket);
 		perror("Bind");
 		exit(EXIT_FAILURE);
 	}
-	printf("Listening...\n");
-	if (listen(my_socket, 10) != 0) {
-		close(my_socket);
-		perror("Listen");
-		exit(EXIT_FAILURE);
-	}
-	
+	return my_socket;
+
+}
+void get_packet(int my_socket) {
 	struct sockaddr_storage incoming_packet;
 	int new_socket_descriptor;
 	
@@ -62,6 +57,20 @@ int main(void) {
 	}
 	printf("%s", buffer);
 	close(new_socket_descriptor);
+
+}
+int main(void) {
+	struct addrinfo* server_info;
+
+	int my_socket = get_port(&server_info);
+
+	printf("Listening...\n");
+	if (listen(my_socket, 10) != 0) {
+		close(my_socket);
+		perror("Listen");
+		exit(EXIT_FAILURE);
+	}
+	get_packet(my_socket);
 	close(my_socket);
 	freeaddrinfo(server_info);
 	return EXIT_SUCCESS;
